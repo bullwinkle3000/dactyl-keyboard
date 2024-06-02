60fb07e2be2159eb43ff4e113a0ea760071b62a3
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-06-02 15:53:44 -0400

    adjusted bottom plate

--- src/helpers_cadquery.py
+++ src/helpers_cadquery.py
@@ -1,7 +1,7 @@
 import cadquery as cq
 from scipy.spatial import ConvexHull as sphull
 import numpy as np
-
+import os
 
 debug_trace = False
 
@@ -230,11 +230,57 @@ def export_file(shape, fname):
 
     export_stl(shape, fname)
 
+puck_height = 3
+puck_r = 41.1 / 2
+base_top_r = puck_r + 2
+base_bottom_r = base_top_r + 5
+base_z_offset = 2
+base_height = puck_height + base_z_offset
+hole_dist = 38.1 / 2
+
+
+def get_puck_base():
+    # main_puck = wp().cylinder(puck_height + 1, puck_r).translate((-65, 0, (puck_height / 2) - 2))
+    base_bottom = cq.Sketch().circle(base_bottom_r)
+    base_top = cq.Sketch().circle(base_top_r)
+    puck_base = wp().placeSketch(base_bottom, base_top.moved(cq.Location(cq.Vector(0, 0, base_height)))).loft()
+    # puck_base = puck_base.translate((-65, 0, (base_height / 2) - 2))
+
+    # holes = [
+    #     [1, 0],
+    #     [0, 1],
+    #     [-1, 0],
+    #     [0, -1]
+    # ]
+    #
+    # hole_shapes = []
+    # for hole in holes:
+    #     hole_shapes.append(wp().cylinder(30, 1.55).translate((hole[0] * hole_dist, hole[1] * hole_dist, base_height)))
+    #
+    # puck_base = difference(puck_base, hole_shapes)
+
+    return puck_base
+
 
 def export_dxf(shape, fname):
     print("EXPORTING TO {}".format(fname))
     cq.exporters.export(w=shape, fname=fname + ".dxf",
                         exportType='DXF')
+
+def mount_plate():
+    height = 7.0
+    result = (
+        wp()
+        .circle(20)
+        .workplane(offset=height)
+        .circle(12)
+        .loft(combine=True)
+    )
+    os.path.abspath(os.path.join(r"src", "parts"))
+
+    screw = cq.importers.importStep(os.path.abspath(os.path.join(r"src", "parts", "quarter_inch_screw.step"))).translate([0, 0, -8])
+
+    return result.cut(screw)  # .translate([0, 0, height / 2.0])
 
 def blockerize(shape):
     #####
@@ -413,7 +459,7 @@ def build_holder(pcb):
     # base = base.cut(pin_row2).cut(pin_row1)
 
     base = base.translate([0, 0, -7.5])
-    holder_hole_width = 28.9
+    holder_hole_width = 29.2
     holder_hole_height = 16.5
     # front wall
 
@@ -427,14 +473,50 @@ def build_holder(pcb):
     wall = wall.cut(inset)
     wall = wall.cut(groove_neg)
     pcb_box = pcb_box.translate([0, 0, -2])
-    posts1 = wp().box(pcb["w"], 3.0, 5.5).cut(wp().box(pcb["w"] - 6, 3.0, 6.0)).translate([0, -pcb["l"] / 2 + 1, -5.8])
-    posts2 = wp().box(pcb["w"], 3.0, 5.5).cut(wp().box(pcb["w"] - 6, 3.0, 6.0)).translate([0, pcb["l"] / 2 - 1, -5.8])
+    # posts1 = wp().box(3, pcb["l"], 5.5).translate([-pcb["w"] / 2 + 1, 0, -5.8])
+    posts2 = wp().box(3, pcb["l"], 5.5).translate([pcb["w"] / 2 - 1, 0, -5.8])
+    rear_guard = wp().box(pcb["w"], 3.0, 7.5).translate([0, -pcb["l"] / 2 - 1, -4.8])
     wall = wall.cut(pcb_box)
-    base = base.union(posts1).union(posts2)
+    base = base.union(posts2)  # .union(rear_guard)
     wall = wall.union(base)
+
+    insert_cap_1 = wp().cylinder(5.5, 3.2).translate((-(pcb["w"] / 2) + 5, -(pcb["l"] / 2) + 4, -5.8))
+    insert_cap_2 = wp().cylinder(5.5, 3.2).translate((-(pcb["w"] / 2) + 5, (pcb["l"] / 2) - 2, -5.8))
+
+    wall = wall.union(insert_cap_1).union(insert_cap_2)
+
+    screw1 = wp().cylinder(20, 1.55).translate((-(pcb["w"] / 2) + 5, -(pcb["l"] / 2) + 4, 0))
+    screw2 = wp().cylinder(20, 1.55).translate((-(pcb["w"] / 2) + 5, (pcb["l"] / 2) - 2, 0))
+
+    wall = wall.cut(screw1).cut(screw2)
 
     return wall
 
 
 def get_holder():
     return build_holder(pcb_v2)
+
+
+def puck_plate():
+    offset = 19.55
+    border = 5
+    border_half = border / 2
+    width = offset * 5
+    height = offset * 4
+
+    # plate = box(width, height, 2)
+
+    plate_bottom = wp().sketch().rect(width, height).edges().fillet(15)
+    plate = plate_bottom.workplane(offset=3).sketch().rect(width / 3, height / 3).fillet(5).loft()
+
+    holes = [
+        [offset, 0, 0],
+        [0, offset, 0],
+        [-offset, 0, 0],
+        [0, -offset, 0]
+    ]
+
+    plate = plate.edges("|Z").fillet(15)
+    # plate = plate.edges(">Y").chamfer(1)
+    plate = plate.faces(">Z").workplane().rect(38.1, 38.1, forConstruction=True).vertices().cboreHole(2.2, 3, 1.5, 3)
+    return plate
